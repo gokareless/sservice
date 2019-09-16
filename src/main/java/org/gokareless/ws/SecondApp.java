654800b471d60c612f7ba541a0c6d6646fcfba41
myan <myan@softserveinc.com>
2019-09-16 14:30:15 +0300

    - Adjusted sservice configs with regard to fservice

--- src/main/java/org/gokareless/ws/SecondApp.java
+++ src/main/java/org/gokareless/ws/SecondApp.java
@@ -1,17 +1,13 @@
 package org.gokareless.ws;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class SecondApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecondApp.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SecondApp.class, args);
+  }
 
 }
